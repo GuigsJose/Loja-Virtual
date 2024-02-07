1c4b1c29f6f6986a3c7ca106d655c12263b0e59b
Guilherme <guizaomtz@gmail.com>
2024-02-07 13:43:48 -0300

    Criação do CRUD na camada de serviço, beta do crud.

--- src/main/java/com/br/services/ClienteService.java
+++ src/main/java/com/br/services/ClienteService.java
@@ -5,6 +5,9 @@ import com.br.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ClienteService {
 
@@ -22,4 +25,35 @@ public class ClienteService {
 
         return clienteRepository.save(cliente);
     }
+
+    public Optional<Cliente> buscarPorId(Long id){
+        return clienteRepository.findById(id);
+    }
+
+    public List<Cliente> buscarTodos(){
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> buscarPorEmail(String email){
+        return clienteRepository.findByEmail(email);
+    }
+
+    public void excluirCliente(Long id){
+        clienteRepository.deleteById(id);
+    }
+
+    public Cliente atualizarCliente(Long id, String nome, String cpf){
+        Optional<Cliente> clienteExistente = clienteRepository.findById(id);
+
+        if (clienteExistente.isPresent()){
+            var cliente = clienteExistente.get();
+            cliente.setNome(nome);
+            cliente.setCpf(cpf);
+            return clienteRepository.save(cliente);
+        }
+        else {
+            return null;
+        }
+    }
+
 }
